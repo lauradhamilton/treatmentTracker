11d52804bf6832b88b72cf5801d5e42858d94fac
Laura Hamilton <lauradhamilton@gmail.com>
2014-05-01 19:15:00 -0500

    Adding a login template but it's not really working

--- js/app.js
+++ js/app.js
@@ -72,6 +72,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
 
+    .state('tab.login', {
+      url: '/login',
+      views: {
+        'tab-login': {
+          templateUrl: 'templates/login.html',
+          controller: 'LoginCtrl'
+        }
+      }
+    })
+
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
